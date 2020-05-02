aa13678efe002567877a02d1854af17fdf943e40
Tomek-dev <siminiak.tomek@gmail.com>
2020-05-02 22:57:53 +0200

    Added index page, header and footer

---
+++ src/main/java/com/app/tictactoe/controller/GameController.java
@@ -0,0 +1,14 @@
+package com.app.tictactoe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GameController {
+
+    @GetMapping("/game")
+    public String normal(Model model){
+        return "game";
+    }
+}
